d8b85e9c404fa654773ad469a30280d040afceaa
shinji <74242033+72-mikan@users.noreply.github.com>
2023-05-24 06:25:39 +0900

    12章まで学習

--- jQuery-Lesson/section7/app.js
+++ jQuery-Lesson/section7/app.js
@@ -1,26 +1,23 @@
 // $(function(){
-//   $('.box1').mouseover(function() {
-//     $('.box1').css({'background-color': '#0000FF'});
+//   $('.bg1').on('click', function() {
+//     $('.bg1').slideUp();
 //   });
-//   $('.box1').mouseout(function() {
-//     $('.box1').css({'background-color': '#FF0000'});
+
+//   $('.bg2').on('click', function() {
+//     $('.bg2').slideUp();
+//   });
+
+//   $('.bg3').on('click', function() {
+//     $('.bg3').slideUp();
+//   });
+
+//   $('.bg4').on('click', function() {
+//     $('.bg4').slideUp();
 //   });
 // });
 
-// $(function(){
-//   $('.box1').mouseover(function() {
-//     $('.box1').addClass('box1-ext');
-//   });
-//   $('.box1').mouseout(function() {
-//     $('.box1').removeClass('box1-ext');
-//   });
-// });
-
-$(function(){
+$(function() {
   $('.box1').on('click', function() {
-    $('.box1').addClass('box1-ext');
-  });
-  $('.box1').mouseout('click', function() {
-    $('.box1').removeClass('box1-ext');
+    $(this).slideUp();
   });
 });
